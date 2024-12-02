99f34532e751114a81511bedf309624b3b2a3896
andrew72-max <andrew.nyongesa@student.moringaschool.com>
2024-12-02 18:39:40 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+
+function UpdateEmployeeWithKeyandValue(employee, key, value){
+    console.log(Object.assign({},employee,{[key]:value}));
+}
+const employee = {
+    name: "Max",
+    streetAddress: "5 Main Street",
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+
+    return Object.assign({}, employee, { [key]: value });
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+  
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const newObj = Object.assign({}, employee);
+  
+    delete newObj[key];
+  
+    return newObj;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+  
+    return employee;
+  }
